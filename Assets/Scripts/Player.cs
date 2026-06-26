6d402addbbc75564ae6ac567d3b08a46447410bd
Ahmed Bahaa‬‏ <body.hammam37@gmail.com>
2026-06-26 06:18:57 +0300

    Player Movement & Visual

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float rotateSpeed = 10f;
+
+    [SerializeField] private GameInput gameInput;
+    private bool isWalking;
+    private void Update()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        
+        isWalking = moveDir != Vector3.zero;
+
+        transform.forward = Vector3.Slerp(transform.forward,moveDir,Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+}
